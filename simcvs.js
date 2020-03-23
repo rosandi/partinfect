79e21841765e5e523624e89e8b63355b8a76ba24
Yudi Rosandi <rosandi@gmail.com>
2020-03-23 17:44:44 +0700

    units is fitted to: time: our distance: meter infection growth time is the time to reach maximum illness level

--- simcvs.js
+++ simcvs.js
@@ -11,28 +11,34 @@
 var canvas = document.getElementById("simcvs");
 var ctx = canvas.getContext("2d");
 
+const maxsick=100;
+const fullangle = 2*Math.PI;
+const clevel = ["#33FF3C", "#86FF33", "#86FF33", "#FFDD33", "#FFDD33", "#FF3C33"];
+const w = canvas.width;
+const h = canvas.height;
+const maxvel=2;
+const maxv2=maxvel*maxvel;
+const simspeed = 20;
+const plotevery = 50;
+
 var npar = 200;
-var w = canvas.width;
-var h = canvas.height;
-var maxvel=2;
-var maxv2=maxvel*maxvel;
-var izone=5;
-var iz2=izone*izone;
-var maxsick=100;
-var clevel = ["#33FF3C", "#86FF33", "#86FF33", "#FFDD33", "#FFDD33", "#FF3C33"];
-var rateinv=5;
-var ninfected=0;
-var infectionlevel=0;
+var rexpos=2;
+var rex2=rexpos*rexpos;
 var maxinfectionlevel=maxsick*npar;
 var worseprobability = 0.75;
 var curepropability = 0.25;
-var ncured = 0;
-var simspeed = 20;
-var plotevery = 50;
 var nstep = 0;
 var initinfect = 1;
 var tincub = 1000;
-var ndead = 0;
+var rateinv=5;
+var delsick=maxsick/rateinv;
+
+//--- measurement variables
+var ndead;
+var noninf;
+var ncured;
+var ninfected;
+var infectionlevel;
 
 var infarr = {
     x: [0],
@@ -62,6 +68,12 @@ var deadarr = {
     name: '#dead'
 };
 
+var noinarr = {
+    x: [0],
+    y: [100],
+    type: 'scatter',
+    name: '#non-infected'
+};
 
 var layout = {
   autosize: false,
@@ -78,7 +90,7 @@ var layout = {
   xaxis: {title: 'time'}
 };
 
-var data=[infarr,curarr,deadarr,levarr];
+var data=[infarr,noinarr,curarr,deadarr,levarr];
 Plotly.newPlot('plot',data,layout);
 
 function parti(x, y, vx, vy, rad, infval) {
@@ -103,42 +115,43 @@ function parti(x, y, vx, vy, rad, infval) {
         } 
     }
     
+    // --- this function defines the disease dynamics ----//
+    
     this.healthcondition = function() {
         if (this.immune) return;
         
         if (this.sick>0) { // if not healthy
             this.infectime++;
             
-            if (this.infectime%rateinv == 0) {
-                
-                if (this.infectime>tincub) {
-                    this.sick--;
-                    // if recovered after infected...
-                    if (this.sick<1) {
-                        this.immune=true;
-                        this.sick=0;
-                    }
-                    return;
+            // what to do if a particle passed incubation time..
+            if (this.infectime>tincub) {
+                this.sick-=delsick;
+                // if recovered after infected...
+                if (this.sick<delsick) {
+                    this.immune=true;
+                    this.sick=0;
                 }
-                
-                // there is a chance that health does not worsen
-                if(Math.random()<worseprobability) {
-                    this.sick++;
-                    if(this.sick > maxsick) {
-                        this.sick=maxsick;
-                        this.dead=true;
-                    }
-                }
-                
-                if(Math.random()<curepropability){
-                    this.sick--;
-                    // if recovered after infected...
-                    if (this.sick<1) {
-                        this.immune=true;
-                        this.sick=0;
-                    }
+                return;
+            }
+            
+            // there is a chance that health does not worsen
+            if(Math.random()<worseprobability) {
+                this.sick+=delsick;
+                if(this.sick > maxsick) {
+                    this.sick=maxsick;
+                    this.dead=true;
                 }
             }
+            
+            if(Math.random()<curepropability){
+                this.sick-=delsick;
+                // if recovered after infected...
+                if (this.sick<delsick) {
+                    this.immune=true;
+                    this.sick=0;
+                }
+            }
+
         }
     }
     
@@ -172,7 +185,7 @@ function parti(x, y, vx, vy, rad, infval) {
         
         if (this.immune) {
             ctx.beginPath();
-            ctx.arc(this.x+this.rad/2,this.y+this.rad/2,this.rad+1,0,2*Math.PI);
+            ctx.arc(this.x+this.rad/2,this.y+this.rad/2,this.rad+1,0,fullangle);
             ctx.stroke();
         }
         
@@ -234,7 +247,7 @@ function checkneigh() {
             dx=p[j].x-p[i].x;
             dy=p[j].y-p[i].y;
             r2=dx*dx+dy*dy;
-            if(r2<iz2) {
+            if(r2<rex2) {
                 p[i].exposed(p[j].sick);
                 p[j].exposed(p[i].sick);
             }
@@ -248,9 +261,11 @@ function detect() {
     infectionlevel=0;
     ncured=0;
     ndead=0;
+    noninf=0;
     
     for (i=0;i<npar;i++) {
         if (p[i].immune) ncured++;
+        if (p[i].infectime==0) noninf++;
         if (p[i].dead) ndead++;
         else {
             // only count alive particles
@@ -260,11 +275,10 @@ function detect() {
     }
     
     document.getElementById("stat").innerHTML=
-    "time = "+nstep+
-    "<br>#infected = "+ninfected+
-    "<br>#cured = "+ncured+
-    "<br>#dead = "+ndead+
-    "<br>infection level = "+(100*infectionlevel/maxinfectionlevel)+"%";
+    "time="+nstep+
+    "<br>non-infected="+noninf+" infected="+ninfected+
+    "<br>cured="+ncured+" dead="+ndead+
+    "<br>infection level = "+(100*infectionlevel/maxinfectionlevel).toFixed(2)+"%";
 }
 
 
@@ -287,11 +301,13 @@ function update() {
         curarr.x.push(nstep);
         deadarr.x.push(nstep);
         levarr.x.push(nstep);
+        noinarr.x.push(nstep);
         
         infarr.y.push(100*ninfected/npar);
         curarr.y.push(100*ncured/npar);
         deadarr.y.push(100*ndead/npar);
         levarr.y.push(100*infectionlevel/maxinfectionlevel);
+        noinarr.y.push(100*noninf/npar);
         Plotly.newPlot('plot',data,layout);
     }
     nstep++;
@@ -330,21 +346,18 @@ function restart() {
     worseprobability = getval('pworse')/100.0;
     curepropability = getval('pcure')/100.0;
     rateinv=getval('rateinv');
-    izone=getval('xrad');
+    rexpos=getval('xrad');
     tincub=getval('tincub');
     initinfect=getval('initinf');
     
-    maxvel=2;
-    maxv2=maxvel*maxvel;
-    iz2=izone*izone;
+    rex2=rexpos*rexpos;
     ninfected=initinfect;
     infectionlevel=0;
     maxinfectionlevel=maxsick*npar;
     ncured = 0;
     nimmune = 0;
-    simspeed = 20;
-    plotevery = 50;
     nstep = 0;
+    delsick=maxsick/rateinv;
     
     infarr.x=[0];
     infarr.y=[0];
@@ -354,6 +367,8 @@ function restart() {
     deadarr.y=[0];
     levarr.x=[0];
     levarr.y=[0];
+    noinarr.x=[0];
+    noinarr.y=[100];
     
     delete p;
     initiate();
